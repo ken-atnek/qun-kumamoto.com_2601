b3d745c6672f9fb912646daa6638cec7bb88b90c
Kenta Urakami <ken.atnek@gmail.com>
2026-01-04 10:27:11 +0900

    Footer構築準備

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,11 +4,28 @@
  * Created: 2025-12-30
  * Last updated: 2025-12-30
  * ======================================= */
+import Image from 'next/image';
 import styles from './Footer.module.scss';
+
 // import Link from 'next/link';
 // import ExternalLink from '@/components/common/ExternalLink';
 const Footer = () => {
-  return <footer className={styles.containerFooter}></footer>;
+  return (
+    <footer className={styles.containerFooter}>
+      <section className={styles.containerContact}>
+        <h2>ご相談・お問い合わせ</h2>
+        <div className={styles.boxLogo}>
+          <Image
+            src="/images/logo-bk.webp"
+            alt="株式会社九州運輸"
+            width={445}
+            height={116}
+            loading="lazy"
+          />
+        </div>
+      </section>
+    </footer>
+  );
 };
 
 export default Footer;
