12efa359f05564d235b8e8935ccb492782720f85
KEN-MBA <165755076+KEN-MBA@users.noreply.github.com>
2026-01-04 13:42:05 +0900

    FOOTER仮組み

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,8 +6,8 @@
  * ======================================= */
 import Image from 'next/image';
 import styles from './Footer.module.scss';
-
-// import Link from 'next/link';
+import ExternalLink from '@/components/common/ExternalLink';
+import Link from 'next/link';
 // import ExternalLink from '@/components/common/ExternalLink';
 const Footer = () => {
   return (
@@ -23,6 +23,115 @@ const Footer = () => {
             loading="lazy"
           />
         </div>
+        <p> お急ぎの方は、お電話にてお問い合わせください。</p>
+        <ExternalLink className={styles.itemTel} href="tel:0968-38-3515">
+          <span>tel.</span>0968-38-3515
+        </ExternalLink>
+        <p>
+          弊社へのご依頼・各種お問い合わせ・求人へのご応募はフォームよりお問い合わせください。
+        </p>
+        <nav>
+          <ExternalLink
+            className={styles.itemAccess}
+            href="https://maps.app.goo.gl/RT2vEvL7KW41Yq5e8"
+          >
+            <h3>Access</h3>
+            <span>熊本県菊池市泗水町田島2724-1</span>
+          </ExternalLink>
+          <Link href="/" className={styles.itemContact}>
+            <h3>Contact us</h3>
+            <span>お問い合わせフォーム</span>
+          </Link>
+        </nav>
+      </section>
+      {/* メニュー */}
+      <section className={styles.containerMenu}>
+        <article>
+          <div className={styles.boxLogo}>
+            <Image
+              src="/images/logo-wh.webp"
+              alt="株式会社九州運輸"
+              width={252}
+              height={28}
+              loading="lazy"
+            />
+          </div>
+          <nav>
+            <Link href="/">保有車両</Link>
+            <Link href="/">サービズ</Link>
+            <Link href="/">会社概要</Link>
+            <Link href="/">採用情報</Link>
+            <Link href="/">各種ご依頼</Link>
+          </nav>
+          <div className={styles.boxAddress}>
+            <span>〒861-1214</span>
+            <span>
+              熊本県菊池市泗水町田島2724-1
+              <ExternalLink href="https://maps.app.goo.gl/RT2vEvL7KW41Yq5e8">
+                MAP
+              </ExternalLink>
+            </span>
+            <div className={styles.wrapTel}>
+              <ExternalLink href="tel:0968-38-3515">
+                <span>tel.</span>0968-38-3515
+              </ExternalLink>
+              <ExternalLink href="tel:0968-38-3715">
+                <span>fax.</span>0968-38-3715
+              </ExternalLink>
+            </div>
+          </div>
+          <div className={styles.boxContact}>
+            <Link href="/">ご依頼・お問い合せ</Link>
+            <p>Copyright 九州運輸. All Rights Reserved.</p>
+          </div>
+        </article>
+      </section>
+      {/* 亀の甲温泉・岡稲荷大明神 */}
+      <section className={styles.containerGroupShop}>
+        <ul>
+          <li>
+            <ExternalLink href="https://kamenokouonsen-kumamoto.com/">
+              <div className={styles.itemImage}>
+                <Image
+                  src="/images/shop-kamenokou.webp"
+                  alt="亀の甲温泉の画像"
+                  width={158}
+                  height={114}
+                />
+              </div>
+              <div className={styles.itemLogo}>
+                <Image
+                  src="/images/logo-kamenokou.webp"
+                  alt="亀の甲温泉の画像"
+                  width={264}
+                  height={67}
+                />
+              </div>
+            </ExternalLink>
+            <p>七水木温泉　岡稲荷大明神を望む　美人の湯。</p>
+          </li>
+          <li>
+            <ExternalLink href="https://kamenokouonsen-kumamoto.com/oka-inari-shrine/">
+              <div className={styles.itemImage}>
+                <Image
+                  src="/images/shop-okainari.webp"
+                  alt="亀の甲温泉の画像"
+                  width={158}
+                  height={114}
+                />
+              </div>
+              <div className={styles.itemLogo}>
+                <Image
+                  src="/images/logo-okainari.webp"
+                  alt="亀の甲温泉の画像"
+                  width={264}
+                  height={67}
+                />
+              </div>
+            </ExternalLink>
+            <p>地域を守り続けて500年 岡稲荷大明神</p>
+          </li>
+        </ul>
       </section>
     </footer>
   );
