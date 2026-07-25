0935d1ea845af8aca6f202e27fa38293977c739a
Ken <ken.atnek@gmail.com>
2026-07-25 14:32:49 +0900

    HEROフェード画像修正

--- src/components/top/ContainerTopHero.tsx
+++ src/components/top/ContainerTopHero.tsx
@@ -12,7 +12,7 @@ import styles from '@/styles/PageTop.module.scss';
 import Image from 'next/image';
 
 const heroImages = [
-  '/images/top/hero01.webp',
+  // '/images/top/hero01.webp',
   // '/images/top/hero02.webp',
   '/images/top/hero03.webp',
 ];
